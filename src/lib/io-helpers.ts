4655bbe53c246fd49eb7f8e7a7049f8ffcc75b3d
liuze <liuze@machloop.com>
2025-04-16 17:07:52 +0800

    feat: 新增takeLeading

--- src/lib/io-helpers.ts
+++ src/lib/io-helpers.ts
@@ -4,17 +4,28 @@ import { RUNNING } from "./taskStatus";
 export function* takeEvery(pattern: string, fn: any, ...args: any[]) {
   while (true) {
     yield take(pattern);
-    yield fork(fn,...args)
+    yield fork(fn, ...args);
   }
 }
 
 export function* takeLatest(pattern: string, fn: any, ...args: any[]) {
-  let lastTask: { status: number; cancel: any } = null;
+  let lastTask: { status: number; cancel: any }|null = null;
   while (true) {
     yield take(pattern);
-    if (lastTask&&lastTask.status === RUNNING) {
+    if (lastTask && lastTask.status === RUNNING) {
       lastTask.cancel();
     }
-    lastTask = yield fork(fn,...args)
+    lastTask = yield fork(fn, ...args);
+  }
+}
+
+export function* takeLeading(pattern: string, fn: any, ...args: any[]) {
+  let firstTask: { status: number; cancel: any }|null = null;
+  while (true) {
+    yield take(pattern);
+    if (firstTask && firstTask.status === RUNNING) {
+      continue;
+    }
+    firstTask = yield fork(fn, ...args);
   }
 }
